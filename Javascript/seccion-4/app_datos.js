5d556cd6f43d29594efa2ac1bb918f63924ca5c4
gomezgleonardob <gomezleonardob@yandex.com>
2019-03-03 19:44:42 -0500

    Add Data Types  and Basic Structures

---
+++ Javascript/seccion-4/app_datos.js
@@ -0,0 +1,18 @@
+//Variables String
+
+let nombre = "Leonardo";
+let apellido = "Gomez";
+
+let nombreCompleto= nombre +' '+apellido;
+console.log(nombreCompleto);
+
+let a = "10";
+let b = "20";
+
+console.log(a+b);
+
+//Variables Booleanas
+
+let activo=true;
+let despedido=false;
+console.log(activo+despedido);
