37fd0926e53ec67302e834822400742934052d66
gomezgleonardob <gomezleonardob@yandex.com>
2019-03-03 21:47:51 -0500

    Add Sections Structures

---
+++ Javascript/seccion-4/app_iterativas_for.js
@@ -0,0 +1,14 @@
+//fOR
+let num = 2;
+
+for(let i=1;i<=10;i++){
+    resultado = num*i;
+    console.log("Tabla del"+' '+num +'*'+i+' '+resultado);
+}
+
+let num1 = 10;
+let limite = 20
+for(let i=1;i<=limite;i++){
+    resultado1 = num1*i;
+    console.log("Tabla del"+' '+num1+'*'+i+' '+resultado1);
+}
---
+++ Javascript/seccion-4/app_iterativas_while.js
@@ -0,0 +1,9 @@
+//
+let gasolina = 100;
+
+while(gasolina>0)
+{
+    console.log("Nivel Tanque"+' ' +gasolina);
+    gasolina--; 
+}
+console.log("Tanque Vacio");
